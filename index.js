a479d0e510382efdea7f6b914efaf7d7aa5e9aee
Thiwanka Somachandra <thivankas@outlook.com>
2024-06-14 17:15:01 +0300

    Adding MongoDB to backend

--- index.js
+++ index.js
@@ -1,83 +1,76 @@
+require('dotenv').config()
 const express = require ('express');
-const cors = require('cors')
+const cors = require('cors');
+const Question = require('./server/question');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-const myQuestions = [
+const myQuestions = [ // initial data to add in to the DB
     {
-        id : 125210,
-        progress : '#ffd64f',
         word : 'Minä',
         correctAnswer : 'Me',
         options : ['She','He','Me','You','They'],
     },
     {
-        id : 125211,
-        progress : '#ffd64f',
         word : 'Sinä',
         correctAnswer : 'You',
         options : ['She','He','Me','You','They']
     },
     {
-        id : 125212,
-        progress : '#ffd64f',
         word : 'Hän',
         correctAnswer : 'She/He',
         options : ['She/He','We','Me','You','They']
     },
     {
-        id : 125213,
-        progress : '#ffd64f',
         word : 'Opettaja',
         correctAnswer : 'Teacher',
         options : ['Officer','Friend','Husband','Teacher','Wife']
     },
     {
-        id : 125214,
-        progress : '#ffd64f',
         word : 'Ystävä',
         correctAnswer : 'Friend',
         options : ['Officer','Friend','Husband','Teacher','Wife']
     },
     {
-        id : 125215,
-        progress : '#ffd64f',
         word : 'Kiitos',
         correctAnswer : 'Thank you!',
         options : ['Fuck you!','Thank you!','Sorry!','Bless you!','Excuse me!']
     },
     {
-        id : 125216,
-        progress : '#ffd64f',
         word : 'Hyvä',
         correctAnswer : 'Good',
         options : ['Bad','God','Happy','Good','Sad']
     },
     {
-        id : 125217,
-        progress : '#ffd64f',
         word : 'Tietokone',
         correctAnswer : 'Computer',
         options : ['Knowledge Box','Computer','Calculator','Mobile Phone','Library']
     },
     {
-        id : 125218,
-        progress : '#ffd64f',
         word : 'Auto',
         correctAnswer : 'Car',
         options : ['Tree','House','Cloth','Table','Car']
     },
     {
-        id : 125219,
-        progress : '#ffd64f',
         word : 'Kirja',
         correctAnswer : 'Book',
         options : ['Milk','Book','Tree','Cow','Pencil']
     },
 ];
+
+async function run () {
+    const question = new Question({
+        word : 'Test',
+        correctAnswer : 'Test',
+        options : ['Test01','Test02','Test03','Test04','Test05']
+    });
+    await question.save();
+    console.log('Question added sucessfully!');
+};
+
+run();
 
 app.get('/',(resquet,response) => {
     response.send('<h1>Hello World</h2>');
@@ -87,7 +80,7 @@ app.get('/api/questions',(resquet,response) => {
     response.status(200).json(myQuestions);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT,() => {
     console.log(`Server running on ${PORT}`);
---
+++ server/question.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery',false);
+const URI = process.env.MONGODB_URI;
+
+console.log('Connecting to the DB...');
+
+async function run () {
+    try {
+        await mongoose.connect(URI);
+        console.log('Sucessfully connected!');
+    } catch (error) {
+        console.log('Something went wrong!',error);
+    }
+}
+
+const questionSchema = new mongoose.Schema({
+    word: String,
+    correctAnswer: String,
+    options: [String],
+});
+
+questionSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+});
+
+run();
+
+module.exports = mongoose.model('Question', questionSchema);
