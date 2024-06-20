7f9222465606dc311c0e387cab492950aed8b11f
Thiwanka Somachandra <thivankas@outlook.com>
2024-06-20 22:43:37 +0300

    Refactoring backend and annd test api

--- index.js
+++ index.js
@@ -7,8 +7,34 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const myQuestions = [
+  {   
+      word : 'Minä',
+      correctAnswer :'Me',
+      options :['She','He','Me','You','They']
+  },
+  {
+      word :'Sinä',
+      correctAnswer :'You',
+      options :['She','He','Me','You','They']
+  },
+  {
+      word :'Hän',
+      correctAnswer : 'She/He',
+      options :['She/He','We','Me','You','They']
+  },
+  {
+      word :'Opettaja',
+      correctAnswer :'Teacher',
+      options :['Officer','Friend','Husband','Teacher','Wife']
+  }, ];
+
 app.get('/',(resquet,response) => {
     response.send('<h1>Finnish English Vocabulary Building App</h2>');
+});
+
+app.get('/test',(resquet,response) => {
+    response.json(myQuestions);
 });
 
 app.get('/api/questions',(resquet,response) => {
@@ -28,27 +54,29 @@ app.get('/api/questions',(resquet,response) => {
 
 app.get('/api/questions/:limit',(request, response) => {
     const limit = parseInt(request.params.limit);
-    Question.aggregate([{ $sample : { size : limit }}])
-    .then(questions =>{
+    async function randomeQuestions (limit) {
+      try {
+        const questions = await Question.aggregate([{ $sample : { size : limit }}])
         if(questions){
-            const result = questions.map(item => {
-                const temp = {
-                    id : item._id,
-                    word : item.word,
-                    correctAnswer : item.correctAnswer,
-                    options : item.options
-                }
-                return temp;
-            })
-            response.json(result);
-        } else {
-            response.status(404).end();
-        }
-    })
-    .catch(error => {
+          const result = questions.map(item => {
+              const temp = {
+                  id : item._id,
+                  word : item.word,
+                  correctAnswer : item.correctAnswer,
+                  options : item.options
+              }
+              return temp;
+          })
+          response.json(result);
+      } else {
+          response.status(404).end();
+      }
+      } catch (error) {
         console.log('Error fetching data : ', error.message)
         response.status(500).end()
-    })
+      }
+    }
+  randomeQuestions(limit);
 });
 
 const PORT = process.env.PORT || 3000;
