2ec028f634f835f1234010b71d7d31ede9f0c5b1
Thiwanka Somachandra <thivankas@outlook.com>
2024-06-21 05:50:55 +0300

    Implementing functionality - Bug fix 01

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
+import NewGame from './NewGame';
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<HomePage />} />
-          <Route path='/new' element={<HomePage />} />
+          <Route path='/new' element={<NewGame />} />
         </Routes>
       </BrowserRouter>
   )
