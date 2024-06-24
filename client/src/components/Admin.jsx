203547d4b1761162bc5bb98ef204e1486402c85f
Thiwanka Somachandra <thivankas@outlook.com>
2024-06-25 01:02:32 +0300

    Implementing data adding #01

---
+++ client/src/components/Admin.jsx
@@ -0,0 +1,7 @@
+const AddData = () => {
+    return (<div>
+        <h1>Hello</h1>
+    </div>);
+}
+
+export default AddData;
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
+import Admin from './Admin';
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<HomePage />} />
+          <Route path='/admin' element={<Admin />} />
         </Routes>
       </BrowserRouter>
   )
 }
 
-export default App
+export default App;
