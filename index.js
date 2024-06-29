5dbf6fe2531f7062da5482079ca7f7e6e3a313d0
Thiwanka Somachandra <thivankas@outlook.com>
2024-06-29 19:31:30 +0300

    Implementing backend testing with jest 07

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const config = require('./utils/config');//Setting up as an env variable
+const config = require('./utils/config');
 const logger = require('./utils/logger');
 
 
