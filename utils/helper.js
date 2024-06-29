cac287bf7211c95877029572d915008acff165a9
Thiwanka Somachandra <thivankas@outlook.com>
2024-06-29 20:56:28 +0300

    Implementing backend testing with jest 13

--- utils/helper.js
+++ utils/helper.js
@@ -2,10 +2,7 @@ const Question = require('../model/question.js');
 
 const initializeDatabase = async (questionList) => {
     await Question.deleteMany({});
-    questionList.map(async (question) => {
-        let questionObject = new Question(question)
-        await questionObject.save();
-    })
+    await Question.insertMany(questionList);
 }; 
 
 module.exports =  {
